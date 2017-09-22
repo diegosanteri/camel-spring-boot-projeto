7012ea8e29d6b39dbc68c601e49b9fe2afb75dd7
Diego Tonini <diegosanteri@gmail.com>
2017-09-22 13:03:41 -0300

    Initial commit

---
+++ src/main/java/br/com/camelspring/service/ProdutoService.java
@@ -0,0 +1,14 @@
+package br.com.camelspring.service;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("file:pedidos").
+        to("activemq:queue:pedidos");
+    }
+}
